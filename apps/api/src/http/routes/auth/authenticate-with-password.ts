2df1d3f9f4dde5ed5007cc721d6238c7fec4d15f
Vinicius Motta <viniciusmotta0806@gmail.com>
2024-05-18 16:04:33 -0300

    authentication functions

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,64 @@
+import { prisma } from '@/lib/prisma'
+import { compare } from 'bcryptjs'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Authenticate with email and password',
+        body: z.object({
+          email: z.string().email(),
+          password: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(400).send({
+          message: 'Invalid email or password.',
+        })
+      }
+
+      if (userFromEmail.passwordHash === null) {
+        return reply
+          .status(400)
+          .send({ message: 'User does not have a password, use social login.' })
+      }
+
+      const isPasswordValid = await compare(
+        password,
+        userFromEmail.passwordHash
+      )
+
+      if (!isPasswordValid) {
+        return reply.status(400).send({
+          message: 'Invalid email or password.',
+        })
+      }
+
+      const token = await reply.jwtSign(
+        {
+          sub: userFromEmail.id,
+        },
+        {
+          sign: {
+            expiresIn: '7d',
+          },
+        }
+      )
+
+      return reply.status(200).send({ token })
+    }
+  )
+}
--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -9,6 +9,8 @@ export async function createAccount(app: FastifyInstance) {
     '/users',
     {
       schema: {
+        tags: ['auth'],
+        summary: 'Create a new user',
         body: z.object({
           name: z.string(),
           email: z.string().email(),
@@ -29,6 +31,12 @@ export async function createAccount(app: FastifyInstance) {
         })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: { domain, shouldAttachUsersByDomain: true },
+      })
+
       const passwordHash = await hash(password, 6)
 
       await prisma.user.create({
@@ -36,6 +44,13 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 
