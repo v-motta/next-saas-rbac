c0c2f323e3a771ee484a989964525adf658aa570
Vinicius Motta <viniciusmotta0806@gmail.com>
2024-05-18 18:17:25 -0300

    get profile feature

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,52 @@
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getProfile(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/profile',
+      {
+        schema: {
+          tags: ['profile'],
+          summary: 'Get authenticated user profile',
+          response: {
+            200: z.object({
+              user: z.object({
+                id: z.string(),
+                name: z.string().nullable(),
+                email: z.string(),
+                avatarUrl: z.string().nullable(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const userId = await request.getCurrentUserId()
+
+        const user = await prisma.user.findUnique({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            avatarUrl: true,
+          },
+          where: {
+            id: userId,
+          },
+        })
+
+        if (!user) {
+          throw new BadRequestError('User not found')
+        }
+
+        return reply.send({ user })
+      }
+    )
+}
