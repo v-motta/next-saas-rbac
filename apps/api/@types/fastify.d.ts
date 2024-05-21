dbc01e41117858903a3656b136b3a20bda9e8337
Vinicius Motta <viniciusmotta0806@gmail.com>
2024-05-20 22:26:17 -0300

    feat: get user membership by slug

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,11 @@
+import type { Member, Organization } from '@prisma/client'
 import 'fastify'
 
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string
+    ): Promise<{ organization: Organization; membership: Member }>
   }
 }
