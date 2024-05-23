feff9b5f3ae414f918b91f1502f669644e7e5112
Vinicius Motta <viniciusmotta0806@gmail.com>
2024-05-23 20:18:19 -0300

    feat: create new project into organization

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -12,6 +12,7 @@ import { getOrganizations } from '@/http/routes/orgs/get-organizations'
 import { shutdownOrganization } from '@/http/routes/orgs/shutdown-organization'
 import { transferOrganization } from '@/http/routes/orgs/transfer-organization'
 import { updateOrganization } from '@/http/routes/orgs/update-organization'
+import { createProject } from '@/http/routes/projects/create-project'
 import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
@@ -80,6 +81,9 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+// project routes
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server listening on http://localhost:${env.SERVER_PORT}`)
