587aa9c758d261f99dabc039360ccb14991582da
Vinicius Motta <viniciusmotta0806@gmail.com>
2024-10-17 07:40:37 -0300

    nova página de criar organização e componentes

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,10 +1,43 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 export function isAuthenticated() {
   return !!cookies().get('token')?.value
+}
+
+export function getCookieOrganization() {
+  return cookies().get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const currentOrganization = getCookieOrganization()
+
+  if (!currentOrganization) {
+    return null
+  }
+
+  const { membership } = await getMembership(currentOrganization)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.id,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -1,7 +1,7 @@
 import { ChevronsUpDown, PlusCircle } from 'lucide-react'
-import { cookies } from 'next/headers'
 import Link from 'next/link'
 
+import { getCookieOrganization } from '@/auth/auth'
 import { getOrganizations } from '@/http/get-organizations'
 
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
@@ -16,7 +16,7 @@ import {
 } from './ui/dropdown-menu'
 
 export async function OrganizationSwitcher() {
-  const cookieOrganization = cookies().get('org')?.value
+  const cookieOrganization = getCookieOrganization()
   const { organizations } = await getOrganizations()
 
   const currentOrganization = organizations.find(
